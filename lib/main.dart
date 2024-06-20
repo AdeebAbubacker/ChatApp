675ee17433a528ce56394a85c4918de8cb04e72b
Adeeb Abubacker <adeebabu655@gmail.com>
2024-06-21 00:19:44 +0530

    main changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:design_implementation/screens/login_screen/screen/login_screen.dart';
 import 'package:design_implementation/screens/view_chat/screen/view_chat_screen.dart';
 import 'package:flutter/material.dart';
-
+//----------------------------------------------------------
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+//--------------------------------------------
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
